813f8352927a6123a8be6f11d5e2eb3fc885769a
lican <1216493487@qq.com>
2019-07-02 10:10:13 +0800

    sss

---
+++ 111.py
@@ -0,0 +1,36 @@
+a = 10
+b = 20
+ 
+if ( a and b ):
+   print ("1 - 变量 a 和 b 都为 true")
+else:
+   print ("1 - 变量 a 和 b 有一个不为 true")
+ 
+if ( a or b ):
+   print ("2 - 变量 a 和 b 都为 true，或其中一个变量为 true")
+else:
+   print ("2 - 变量 a 和 b 都不为 true")
+ 
+# 修改变量 a 的值
+a = 0
+if ( a and b ):
+   print ("3 - 变量 a 和 b 都为 true")
+else:
+   print ("3 - 变量 a 和 b 有一个不为 true")
+ 
+if ( a or b ):
+   print ("4 - 变量 a 和 b 都为 true，或其中一个变量为 true")
+else:
+   print ("4 - 变量 a 和 b 都不为 true")
+ 
+if not( a and b ):
+   print ("5 - 变量 a 和 b 都为 false，或其中一个变量为 false")
+else:
+   print ("5 - 变量 a 和 b 都为 true") 
+i = sum = 0
+while i <= 4:
+    sum += i
+    i = i+1
+print(sum)
+while 4 == 4:
+    print('4')
